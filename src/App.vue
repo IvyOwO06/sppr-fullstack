72f720d859df4533ed1d9cf6e2b9b15c1279f2a3
Sam <samdelhaye06@gmail.com>
2026-09-17 11:27:41 +0200

    Create Vue starter homepage

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,45 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+const name = ref('Ivy')
+const message = ref('Welcome to my Vue website!')
+
+function changeMessage() {
+  message.value = 'You clicker the button!'
+}
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <main>
+    <h1>My Vue Website</h1>
+
+    <p>{{ message }}</p>
+
+    <p>Hello, {{ name }}!</p>
+
+    <button @click="changeMessage">Click me</button>
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+main {
+  max-width: 800px;
+  margin: 50px auto;
+  text-align: center;
+  font-family: Arial, sans-serif;
+}
+
+h1 {
+  font-size: 40px;
+}
+
+p {
+  font-size: 20px;
+}
+
+button {
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+}
+</style>
