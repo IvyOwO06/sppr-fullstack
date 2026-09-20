62bea080a8510b612dd86b68929bda10963f22cf
Sam <samdelhaye06@gmail.com>
2026-09-20 12:36:20 +0200

    finished sprint 3

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <script setup>
-import Header from './components/header.vue'
+import Header from './components/Header.vue'
 import CounterCard from './components/CounterCard.vue'
-import InfoCard from './components/InfoCard.vue'
+import CharacterCard from './components/CharacterCard.vue'
+
+import { ref } from 'vue'
+
+const message = ref('Choose a character to interact with!')
+
+function handleInteraction(characterName) {
+  message.value = `You interacted with ${characterName}!`
+}
 </script>
 
 <template>
@@ -10,18 +18,44 @@ import InfoCard from './components/InfoCard.vue'
   <main>
     <CounterCard />
 
-    <InfoCard />
-    <InfoCard />
+    <div class="characters">
+      <CharacterCard name="Kris" :hp="90" @interact="handleInteraction" />
+
+      <CharacterCard name="Susie" :hp="120" @interact="handleInteraction" />
+
+      <CharacterCard name="Ralsei" :hp="70" @interact="handleInteraction" />
+    </div>
+
+    <p class="message">* {{ message }}</p>
   </main>
 </template>
 
 <style scoped>
 main {
-  max-width: 800px;
+  max-width: 1000px;
   margin: 40px auto;
   padding: 0 20px;
-  display: flex;
-  flex-direction: column;
+}
+
+.characters {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
   gap: 20px;
+  margin-top: 30px;
+}
+
+.message {
+  margin-top: 30px;
+  padding: 15px;
+  border: 2px solid white;
+  background-color: black;
+  color: white;
+  text-align: center;
+}
+
+@media (max-width: 700px) {
+  .characters {
+    grid-template-columns: 1fr;
+  }
 }
 </style>
---
+++ src/components/CharacterCard.vue
@@ -0,0 +1,49 @@
+<script setup>
+defineProps({
+  name: String,
+  hp: Number,
+})
+
+const emit = defineEmits(['interact'])
+
+function interact() {
+  emit('interact', name)
+}
+</script>
+
+<template>
+  <div class="character-card">
+    <h2>{{ name }}</h2>
+
+    <p>HP: {{ hp }}</p>
+
+    <button @click="interact">Interact</button>
+  </div>
+</template>
+
+<style scoped>
+.character-card {
+  border: 2px solid white;
+  padding: 20px;
+  background-color: black;
+  color: white;
+  text-align: center;
+}
+
+.character-card h2 {
+  margin-top: 0;
+}
+
+button {
+  background-color: black;
+  color: white;
+  border: 2px solid white;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: white;
+  color: black;
+}
+</style>
